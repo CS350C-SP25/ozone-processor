8dcad0a7f534418bba5c8b1c99e08b35e514bb4d
txst54 <wang1024x768@gmail.com>
2025-04-05 02:27:25 -0500

    commit done for ROB

--- src/backend/reorder_buffer.sv
+++ src/backend/reorder_buffer.sv
@@ -1,4 +1,5 @@
 import uop_pkg::*;
+import reg_pkg::*;
 import rob_pkg::*;
 
 // Q_WIDTH must be at least as large as Super scalar * max crack size
@@ -59,10 +60,82 @@ endmodule: reorder_buffer_queue
 
 module reorder_buffer #(
     parameter ADDR_BITS = 64,
+    parameter WORD_SIZE
 ) (
+    input logic clk_in,
+    input logic rst_N_in,
     input rob_entry [Q_WIDTH-1:0] q_in,
     input logic [$clog2(Q_WIDTH+1)-1:0] enq_in,
-    output logic valid_pc, // if PC needs to be set for exception handling, branch mispredictions, trap, etc..
-    output logic [ADDR_BITS-1:0] pc_out
+    output logic valid_pc_out, // if PC needs to be set for exception handling, branch mispredictions, trap, etc..
+    output logic [ADDR_BITS-1:0] pc_out,
+    output logic[Q_WIDTH-1:0] valid_str_out, // map of which stores are valid
+    output logic [Q_WIDTH-1:0][$clog2(reg_pkg::NUM_ARCH_REGS)-1:0] str_addr_reg_out, // arch reg to load STR addr from
+    output logic [Q_WIDTH-1:0][$clog2(reg_pkg::NUM_ARCH_REGS)-1:0] str_addr_reg_off_out, // arch reg to load STR addr from
+    output logic [Q_WIDTH-1:0][$clog2(reg_pkg::NUM_ARCH_REGS)-1:0] str_val_reg_out // arch reg to load STR val from
 );
+    // ** REORDER_BUFFER_QUEUE PARAMS **
+    // queue input
+    logic flush_in;
+    logic [$clog2(Q_WIDTH+1)-1:0] deq_in;
+    // queue output
+    rob_entry [Q_WIDTH-1:0] queue_out;
+    logic queue_full;
+    logic queue_empty;
+    logic [$clog2(Q_DEPTH)-1:0] queue_size;
+
+    reorder_buffer_queue #(
+        .Q_DEPTH(rob_pkg::ROB_ENTRIES),
+        .Q_WIDTH(uop_pkg::INSTR_Q_WIDTH)
+    ) reorder_buffer_queue_internal(
+        // ** INPUTS ** 
+        clk_in,
+        rst_N_in,
+        flush_in,
+        q_in,
+        enq_in,
+        deq_in,
+        // ** OUTPUTS **
+        queue_out,
+        queue_full,
+        queue_empty,
+        queue_size
+    );
+    always_ff @(posedge clk_in) begin : reorder_buffer_fsm
+        if (rst_N_in) begin // not reset
+        end
+    end
+
+    always_comb begin
+        // ** CHECK COMMIT **
+        deq_in = 0;
+        if (!queue_empty & queue_size >= uop_pkg::INSTR_Q_WIDTH) begin
+            for (int i = 0; i < uop_pkg::INSTR_Q_WIDTH; i++) begin
+                case (queue_out[i])
+                    DONE: begin
+                        if (queue_out[i].uop_code == UOP_STORE) begin // only str on commit
+                            valid_str_out[i] = 1'b1;
+                            str_addr_reg_out[i] = queue_out[i].data.dest_reg.gpr;
+                            str_addr_reg_off_out[i] = queue_out[i].data.src2.gpr;
+                            str_val_reg_out[i] = queue_out[i].data.src1.gpr;
+                        end
+                        deq_in += 1;
+                        // TODO update RRAT mapping to match architectural state
+                    end
+                    EXCEPTION: begin
+                        // TODO set PC to exception handler
+                        break;
+                    end
+                    INTERRUPT: begin
+                        // TODO set PC to interrupt handler
+                        break;
+                    end
+                    TRAP: begin
+                        // TODO set PC to exception handler to invoke privileged exec mode
+                        break;
+                    end
+                    default: break;
+                endcase
+            end
+        end
+    end
 endmodule: reorder_buffer
