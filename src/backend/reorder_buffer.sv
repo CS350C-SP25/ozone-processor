cd6e4c8e39de39dded36957072af4f81cdb1be18
txst54 <wang1024x768@gmail.com>
2025-04-05 15:35:12 -0500

    rob insn for exec

--- src/backend/reorder_buffer.sv
+++ src/backend/reorder_buffer.sv
@@ -13,11 +13,13 @@ module reorder_buffer_queue #(
     input rob_entry [Q_WIDTH-1:0] q_in,
     input logic [$clog2(Q_WIDTH+1)-1:0] enq_in, // how many to push IMPORTANT, IT IS ENQERS JOB TO DETERMINE HOW MANY IS SAFE TO ENQ
     input logic [$clog2(Q_WIDTH+1)-1:0] deq_in, // how many to pop IMPORTANT, IT IS DEQERS JOB TO DETERMINE HOW MANY IS SAFE TO DEQ (USE SIZE)
+    input logic [$clog2(Q_DEPTH)-1:0] idx_in,   // for querying, fully combinational
 
-    output rob_entry [Q_WIDTH-1:0] q_out,        // the top width elements of the queue
+    output rob_entry [Q_WIDTH-1:0] q_out,       // the top width elements of the queue
     output logic full,                          // 1 if the queue is full
     output logic empty,                         // 1 if the queue is empty
-    output logic [$clog2(Q_DEPTH)-1:0] size    // the #elems in the queue
+    output logic [$clog2(Q_DEPTH)-1:0] size,    // the #elems in the queue
+    output rob_entry entry_out                  // for querying, returns queue[idx_in]
 ); 
     rob_entry [Q_DEPTH-1:0] q;
     logic [$clog2(Q_DEPTH)-1:0] head;
@@ -45,6 +47,7 @@ module reorder_buffer_queue #(
         q_next = q_in;
         size_incr = flush_in ? 0 : enq_in;
         size_decr = flush_in ? tail - head : deq_in;
+        entry_out = q[idx_in + head];
     end
     generate
         genvar i;
@@ -77,11 +80,16 @@ module reorder_buffer #(
     // queue input
     logic flush_in;
     logic [$clog2(Q_WIDTH+1)-1:0] deq_in;
+    logic [$clog2(Q_DEPTH)-1:0] idx_in;
     // queue output
     rob_entry [Q_WIDTH-1:0] queue_out;
     logic queue_full;
     logic queue_empty;
     logic [$clog2(Q_DEPTH)-1:0] queue_size;
+    rob_entry queue_entry_out;
+
+    // ** INTERNAL LOGISTIC WIRES **
+    uop_rr cur_uop;
 
     reorder_buffer_queue #(
         .Q_DEPTH(Q_DEPTH),
@@ -94,51 +102,72 @@ module reorder_buffer #(
         q_in,
         enq_in,
         deq_in,
+        idx_in,
         // ** OUTPUTS **
         queue_out,
         queue_full,
         queue_empty,
-        queue_size
+        queue_size,
+        queue_entry_out
     );
+
     always_ff @(posedge clk_in) begin : reorder_buffer_fsm
         if (rst_N_in) begin // not reset
         end
     end
 
     always_comb begin
-        // ** CHECK COMMIT **
         deq_in = 0;
         str_val_reg_out = '0;
         str_addr_reg_off_out = '0;
         str_addr_reg_out = '0;
         valid_str_out = '0;
-        if (!queue_empty & queue_size >= uop_pkg::INSTR_Q_WIDTH) begin
-            for (int i = 0; i < uop_pkg::INSTR_Q_WIDTH; i++) begin
-                case (queue_out[i])
-                    DONE: begin
-                        if (queue_out[i].uop.uopcode == UOP_STORE) begin // only str on commit
-                            valid_str_out[i] = 1'b1;
-                            str_addr_reg_out[i] = queue_out[i].uop.data.dst.gpr;
-                            str_addr_reg_off_out[i] = queue_out[i].uop.data.src2.gpr;
-                            str_val_reg_out[i] = queue_out[i].uop.data.src1.gpr;
+        cur_uop = '0;
+        if (!queue_empty) begin
+            // ** INSTRUCTION WINDOW COMMIT **
+            if (queue_size >= uop_pkg::INSTR_Q_WIDTH) begin
+                for (int i = 0; i < uop_pkg::INSTR_Q_WIDTH; i++) begin
+                    case (queue_out[i])
+                        DONE: begin
+                            if (queue_out[i].uop.uopcode == UOP_STORE) begin // only str on commit
+                                valid_str_out[i] = 1'b1;
+                                get_data_rr(queue_out[i].uop.data, cur_uop);
+                                str_addr_reg_out[i] = cur_uop.dst.gpr;
+                                str_addr_reg_off_out[i] = cur_uop.src2.gpr;
+                                str_val_reg_out[i] = cur_uop.src1.gpr;
+                            end
+                            deq_in += 1;
+                            // TODO update RRAT mapping to match architectural state
                         end
-                        deq_in += 1;
-                        // TODO update RRAT mapping to match architectural state
-                    end
-                    EXCEPTION: begin
-                        // TODO set PC to exception handler
-                        break;
-                    end
-                    INTERRUPT: begin
-                        // TODO set PC to interrupt handler
-                        break;
-                    end
-                    TRAP: begin
-                        // TODO set PC to exception handler to invoke privileged exec mode
-                        break;
-                    end
-                    default: break;
-                endcase
+                        EXCEPTION: begin
+                            // TODO set PC to exception handler
+                            break;
+                        end
+                        INTERRUPT: begin
+                            // TODO set PC to interrupt handler
+                            break;
+                        end
+                        TRAP: begin
+                            // TODO set PC to exception handler to invoke privileged exec mode
+                            break;
+                        end
+                        default: break;
+                    endcase
+                end
+            end
+            // ** PROVIDE ISSUE INSN OPTIONS FOR EXEC **
+            for (int i = 0; i < queue_size; i++) begin
+                idx_in = i;
+                if (
+                    queue_entry_out.uop.uopcode == UOP_ADD || 
+                    queue_entry_out.uop.uopcode == UOP_SUB
+                    ) begin
+                    end else if (
+                        queue_entry_out.uop.uopcode == UOP_FADD || 
+                        queue_entry_out.uop.uopcode == UOP_FSUB || 
+                        queue_entry_out.uop.uopcode == UOP_FMUL || 
+                        queue_entry_out.uop.uopcode == UOP_FNEG
+                    )
             end
         end
     end
