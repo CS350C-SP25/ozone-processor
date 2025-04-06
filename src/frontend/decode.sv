b77bfe2091db217aec602caa68d5b6cd311dc39c
XuJeff04 <jeffery.xu.2023@gmail.com>
2025-04-05 19:35:16 -0500

    decode sends pc

--- src/frontend/decode.sv
+++ src/frontend/decode.sv
@@ -10,11 +10,12 @@ module decode #(
     input logic clk_in,
     input logic rst_N_in,
     input logic flush_in,
-    input logic[SUPER_SCALAR_WIDTH-1:0][INSTRUCTION_WIDTH-1:0] fetched_ops,
+    input logic [SUPER_SCALAR_WIDTH-1:0][INSTRUCTION_WIDTH-1:0] fetched_ops,
     input opcode_t [SUPER_SCALAR_WIDTH-1:0] opcode,
     input uop_branch [SUPER_SCALAR_WIDTH-1:0] branch_data,
+    input logic [SUPER_SCALAR_WIDTH-1:0][63:0] pc,
     input logic fetch_valid,
-    input logic exe_ready;
+    input logic exe_ready,
     output logic decode_ready,
     output logic decode_valid,
     output logic [$clog2(INSTR_Q_WIDTH+1)-1:0] instruction_queue_pushes,
@@ -79,7 +80,7 @@ module decode #(
             ri.src.gpr = op_bits[9:5];
             ri.src.is_sp = &op_bits[9:5];
             ri.src.is_fp = '0;
-            ri.imm = {9'b0, op_bits[20:12]};
+            ri.imm = {12'b0, op_bits[20:12]};
             ri.set_nzcv = '0;
             set_data_ri(ri, uop_out.data);
             uop_out.valb_sel = '0 //use imm (there is no src2)
@@ -102,7 +103,7 @@ module decode #(
             ri.src.gpr = op_bits[20:12] == 0 ? op_bits[9:5] : op_bits[4:0]; //if we add we store the add result into dst, then we reuse dst
             ri.src.is_sp = op_bits[20:12] == 0 ? &op_bits[9:5] : '0;
             ri.src.is_fp = '0;
-            ri.imm = 18'b0;
+            ri.imm = 21'b0;
             ri.hw = 2'b0;
             ri.set_nzcv = '0;
             set_data_ri(ri, uop_out.data);
@@ -120,7 +121,30 @@ module decode #(
             ri.src.gpr = 5'b0;
             ri.src.is_sp = '0;
             ri.src.is_fp = '0;
-            ri.imm = {3'b0, op_bits[20:5]};
+            ri.imm = {5'b0, op_bits[20:5]};
+            ri.set_nzcv = '0;
+            set_data_ri(ri, uop_out.data);
+            uop_out.valb_sel = '0;
+            uop_out.mem_read = '0;
+            uop_out.mem_write = '0;
+            uop_out.w_enable = '1;
+            uop_out.tx_begin = 1'b1;
+            uop_out.tx_end = 1'b1;
+        endfunction
+
+        function automatic void decode_i2_format(
+            input logic[INSTRUCTION_WIDTH-1:0] op_bits,
+            output uop_insn uop_out
+        );
+            uop_ri ri;
+            ri.dst.gpr = op_bits[4:0];
+            ri.dst.is_sp = '0;
+            ri.dst.is_fp = '0;
+
+            ri.src.gpr = 5'b0;
+            ri.src.is_sp = '0;
+            ri.src.is_fp = '0;
+            ri.imm = {op_bits[30:29], op_bits[23:5]};
             ri.set_nzcv = '0;
             set_data_ri(ri, uop_out.data);
             uop_out.valb_sel = '0;
@@ -165,7 +189,7 @@ module decode #(
             ri.src.gpr = op_bits[9:5];
             ri.src.is_sp = &op_bits[9:5] & (op_bits[31:22] == 10'b1001000100 | op_bits[31:22] == 10'b1101000100); //ADD or SUB
             ri.src.is_fp = op_bits[26];
-            ri.imm = {6'b0, op_bits[21:10]};
+            ri.imm = {9'b0, op_bits[21:10]};
             ri.set_nzcv = '0;
             set_data_ri(ri, uop_out.data);
             uop_out.valb_sel = '0;
@@ -196,6 +220,7 @@ module decode #(
                         enq_next[enq_idx].mem_read = opcode[instr_idx] == OPCODE_LDUR || opcode[instr_idx] == OPCODE_F_LDUR;
                         enq_next[enq_idx].mem_write = opcode[instr_idx] == OPCODE_STUR || opcode[instr_idx] == OPCODE_F_STUR;
                         enq_next[enq_idx].w_enable = opcode[instr_idx] == OPCODE_LDUR || opcode[instr_idx] == OPCODE_F_LDUR;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     // Immediate moves
@@ -204,62 +229,70 @@ module decode #(
                         decode_i1_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = opcode[instr_idx] == OPCODE_MOVK ? OPCODE_MOVK : OPCODE_MOVZ;
                         enq_next[enq_idx].hw = opcode[instr_idx] == OPCODE_MOVK ? opcode[instr_idx][22:21] : 2'b0;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_ADRP:
-                        enq_next[enq_idx].uop_code = UOP_ADRP_MOV;
-                        enq_next[enq_idx].data = branch_data[instr_idx]; // NOTE THIS DATA IS MILDLY DIFFERENT FROM BRANCHES (IT CONTAINS A REGISTER NOT A COND)
-                        enq_next[enq_idx].valb_sel = '0;
-                        enq_next[enq_idx].tx_begin = 1'b1;
-                        enq_next[enq_idx].tx_end = 1'b1;
+                        decode_i2_format(opcode[instr_idx], enq_next[enq_idx]);
+                        enq_next[enq_idx].pc = pc[instr_idx];
+                        enq_idx = enq_idx + 1;
                         break;
                     // Integer ALU operations
                     OPCODE_ADD:
                         decode_ri_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_ADD;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_ADDS:
                         ecode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_ADD;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_CMN:
                         decode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_ADD;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_next[enq_idx].dst.gpr = 5'h1f; // we discard the destination even though dst could? be non 0
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_SUB:
                         decode_ri_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_SUB;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_SUBS:
                     OPCODE_CMP:
                         ecode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_SUB; //the dst is all 1s unlike cmn.
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_MVN:
                         ecode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_MVN;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_ORR:
                         ecode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_ORR;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_EOR:
                         ecode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_EOR;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_ANDS:
                     OPCODE_TST:
                         ecode_rr_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_AND;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_LSL:
@@ -267,29 +300,45 @@ module decode #(
                     OPCODE_UBFM:
                         decode_ri_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_UBFM;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_ASR:
                         decode_ri_format(opcode[instr_idx], enq_next[enq_idx]);
                         enq_next[enq_idx].uop_code = UOP_ASR;
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_idx = enq_idx + 1;
                         break;
                     // Branching
                     OPCODE_B:
+                        //this has no impact on the architectural state after fetch / predecode
+                        break;
                     OPCODE_B_COND:
-                    OPCODE_BL:
-                        enq_next[enq_idx].uop_code = UOP_BRANCH;
-                        enq_next[enq_idx].data = branch_data[instr_idx];
+                        enq_next[enq_idx].uop_code = UOP_BCOND;
+                        enq_next[enq_idx].data = branch_data[instr_idx]; // matches the uop_branch struct
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_next[enq_idx].valb_sel = '0;
                         enq_next[enq_idx].tx_begin = 1'b1;
                         enq_next[enq_idx].tx_end = 1'b1;
+                        enq_idx = enq_idx + 1;
+                        break;
+                    OPCODE_BL:
+                        enq_next[enq_idx].uop_code = UOP_BL;
+                        enq_next[enq_idx].data = branch_data[instr_idx]; //the branch target and the lower 5 bits are the register to store to (X30)
+                        enq_next[enq_idx].pc = pc[instr_idx];
+                        enq_next[enq_idx].valb_sel = '0;
+                        enq_next[enq_idx].tx_begin = 1'b1;
+                        enq_next[enq_idx].tx_end = 1'b1;
+                        enq_idx = enq_idx + 1;
                         break;
                     OPCODE_RET:
                         enq_next[enq_idx].uop_code = UOP_CHECK_RET;
-                        enq_next[enq_idx].data = branch_data[instr_idx]; // NOTE THIS DATA IS MILDLY DIFFERENT FROM BRANCHES (IT CONTAINS A REGISTER NOT A COND)
+                        enq_next[enq_idx].data = branch_data[instr_idx]; // the bottom 5 bits contain the register to return from
+                        enq_next[enq_idx].pc = pc[instr_idx];
                         enq_next[enq_idx].valb_sel = '0;
                         enq_next[enq_idx].tx_begin = 1'b1;
                         enq_next[enq_idx].tx_end = 1'b1;
+                        enq_idx = enq_idx + 1;
                         break;
                     // Misc
                     OPCODE_NOP:
@@ -297,7 +346,7 @@ module decode #(
                     OPCODE_HLT:
                         enq_next[enq_idx].uop_code = UOP_HLT;
                         enq_next[enq_idx].tx_begin = 1'b1;
-                        enq_next[enq_idx].tx_end = 1'b1
+                        enq_next[enq_idx].tx_end = 1'b1;
                         enq_idx = enq_idx + 1;
                         break;
                     OPCODE_FMOV:
