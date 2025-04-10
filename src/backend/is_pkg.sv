11e2be96ef0517791e60b80b378f315017b8c88d
Tvisha Andharia <tvisha.andharia@gmail.com>
2025-04-10 17:53:58 -0500

    deleting

--- src/backend/is_pkg.sv
+++ src/backend/is_pkg.sv
@@ -7,14 +7,8 @@ package is_pkg;
     parameter int NUM_BRU = 1;
     parameter int NUM_ALU = 1;
     parameter int NUM_FPU = 1;
-    
+
     parameter int FQ_EL_SIZE = $bits(rob_issue);
     parameter int FQ_ENTRIES = ROB_ENTRIES / 4;
-
-    typedef enum logic [1:0] {
-        WAITING,
-        IN_QUEUE,
-        ISSUED
-    } instr_status;
 
 endpackage
