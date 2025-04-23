284a7a89cfc435b6af25d52b5b76fa54ef598c6f
Edward Chamblee <edwardchamblee@gmail.com>
2025-04-23 17:53:18 -0500

    Made most logic combinational rather than sequential

--- src/backend/frl.sv
+++ src/backend/frl.sv
@@ -15,10 +15,10 @@ module frl #(
     input   logic [5:0][$clog2(NUM_PHYS_REGS) - 1:0] freeing_registers
 );
 
-  logic [NUM_PHYS_REGS-1:0] [$clog2(NUM_PHYS_REGS) - 1:0] phys_regs; // The queue. Physical registers are stored at indices, 
+  logic [NUM_PHYS_REGS-1:0] [$clog2(NUM_PHYS_REGS) - 1:0] phys_regs, // The queue. Physical registers are stored at indices, 
                                                                      // not necessarily in order.
                                                           phys_reg_indices; // Physical register mapping. Physical register as index,
-                                                                            // queue index stored at that index
+                                                                          // queue index stored at that index
   
   logic [$clog2(NUM_PHYS_REGS)-1:0] head, // Points to the next free entry. Points to tail when the queue is empty or full.
                                     tail, // Points to the next entry to free
@@ -40,6 +40,64 @@ module frl #(
            If not, set acquire_valid_out to 0 (stall)
   */
 
+  always_comb begin
+
+      // Check which registers were accepted on previous cycle
+      // We will swap an accepted register to the tail of the queue
+      // We will know which registers this corresponds to using the index of acquire_ready_in as the offset from head
+      
+      // Free registers
+      // WARNING: Will break if you're freeing registers that were never acquired!
+      for (int i = 0; i < 6; i++) begin
+        if (free_valid_in[i]) begin
+          
+          // Swap places in queue
+          temp_index = phys_reg_indices[freeing_register[i]]; // Store the queue index of the register we are freeing
+
+          // Swap the indices of the registers whose places in the queue we are swapping
+          phys_reg_indices[freeing_register[i]] = tail; // Change freeing register's index to the tail
+          phys_reg_indices[phys_regs[tail]] = temp_index; // Change index of register at tail to that of the freeing register
+
+          // Swap register places in queue
+          phys_regs[temp_index] = phys_regs[tail]; // Move tail to freeing register's index
+          phys_regs[tail] = freeing_register[i]; // Move freeing register to tail's index
+          
+          tail = tail + 1; // Increment tail
+          if (head == tail) begin
+            empty = '1;
+          end
+        end
+      end
+
+      // WARNING: This will break if the lowest order register signals were not used first!
+      // For each register that could have been acquired
+      for (int i = 0; i < 6; i++) begin
+        if (acquire_ready_in[i]) begin // If this register was acquired
+          head = head + 1;
+          empty = '0;
+        end
+      end
+
+      // Logic for computing number of free registers
+      if (tail > head) begin
+        num_free_regs = head + ({(NUM_PHYS_REGS-1){1'b1}} - tail); // Parentheses to not overflow head before subtracting tail
+      end else if (!empty) begin
+        num_free_regs = head - tail;
+      end else begin
+        num_free_regs = {(NUM_PHYS_REGS-1){1'b1}}; // Max value, all registers are free
+      end
+
+      if (num_free_regs < 6) begin
+        acquire_valid_out = '0; // Stall
+      end else begin
+        // For each free register to provide
+        for (int i = 0; i < 6; i++) begin
+          registers_out[i] = head + i;
+        end
+      end
+
+  end
+
   always_ff @(posedge clk) begin
 
     if (rst) begin
@@ -54,68 +112,7 @@ module frl #(
       tail <= '0;
       empty <= '1;
 
-    end else begin
-    
-      // Check which registers were accepted on previous cycle
-      // We will swap an accepted register to the tail of the queue
-      // We will know which registers this corresponds to using the index of acquire_ready_in as the offset from head
-      
-      // Free registers
-      // WARNING: Will break if you're freeing registers that were never acquired!
-      for (int i = 0; i < 6; i++) begin
-        if (free_valid_in[i]) begin
-          
-          // TODO: This needs to be combinational!
-
-          // Swap places in queue
-          temp_index <= phys_reg_indices[freeing_register[i]]; // Store the queue index of the register we are freeing
-
-          // Swap the indices of the registers whose places in the queue we are swapping
-          phys_reg_indices[freeing_register[i]] <= tail; // Change freeing register's index to the tail
-          phys_reg_indices[phys_regs[tail]] <= temp_index; // Change index of register at tail to that of the freeing register
-
-          // Swap register places in queuequeue
-          phys_regs[temp_index] <= phys_regs[tail]; // Move tail to freeing register's index
-          phys_regs[tail] <= freeing_register[i]; // Move freeing register to tail's index
-          
-          tail <= tail + 1; // Increment tail
-          if (head == tail) begin
-            empty <= '1;
-          end
-        end
-      end
-
-      // WARNING: This will break if the lowest order register signals were not used first!
-      // For each register that could have been acquired
-      for (int i = 0; i < 6; i++) begin
-        if (acquire_ready_in[i]) begin // If this register was acquired
-          head <= head + 1;
-          empty <= '0;
-        end else begin
-          break;
-        end
-      end
-
-      // Logic for computing number of free registers
-      if (tail > head) begin
-        num_free_regs <= head + ({(NUM_PHYS_REGS-1){1'b1}} - tail); // Parentheses to not overflow head before subtracting tail
-      end else if (!empty) begin
-        num_free_regs <= head - tail;
-      end else begin
-        num_free_regs <= {(NUM_PHYS_REGS-1){1'b1}}; // Max value, all registers are free
-      end
-      
-      if (num_free_regs < 6) begin
-        acquire_valid_out <= '0; // Stall
-      end else begin
-        // For each free register to provide
-        for (int i = 0; i < 6; i++) begin
-          registers_out[i] <= head + i;
-        end
-      end
-
     end
-
   end
 
 endmodule
