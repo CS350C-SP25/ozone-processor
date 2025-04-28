e4bbeb82dcb1aea84d4ac886add20cbfda99177e
Dhairya Gupta <dhairyag23@gmail.com>
2025-04-28 18:27:54 -0500

    pls

--- src/frontend/decode.sv
+++ src/frontend/decode.sv
@@ -385,10 +385,11 @@ module decode #(
         end
         $display("fetched_ops: %x", fetched_ops);
         //debug
-        if (enq_idx > 0) begin
-                            $display("[Decode] Instr %d at PC 0x%h: opcode=%s, instr=0x%h", instr_idx,
-                                 enq_next[enq_idx-1].pc, enq_next[enq_idx-1].uopcode.name(), fetched_ops[instr_idx]);
-                        end
+        // if (enq_idx > 0) begin
+        //   $display("[Decode] Instr %d at PC 0x%h: opcode=%s, instr=0x%h", instr_idx,
+        //            enq_next[enq_idx-1].pc, enq_next[enq_idx-1].uopcode.name(),
+        //            fetched_ops[instr_idx]);
+        // end
         //                 //debug
 
 
